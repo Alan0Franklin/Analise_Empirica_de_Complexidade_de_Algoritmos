7a6c9635053760031caa39fdbae986188a709644
Alan Franklin <167710977+Alan0Franklin@users.noreply.github.com>
2025-09-19 19:32:45 -0300

    Update busca_binaria.cpp
    Adaptado para o Caso Médio.

--- src_cpp/busca_binaria.cpp
+++ src_cpp/busca_binaria.cpp
@@ -13,4 +13,5 @@ bool busca_binaria(std::vector<int> &vetor, int valor_alvo, int tamanho_vetor){
 }
 
 // Algoritmo de Busca Binário adaptado para std::function<void(std::vector<int> &vetor, int tamanho_vetor)>.
-void busca_binaria(std::vector<int> &vetor, int tamanho_vetor){busca_binaria(vetor, tamanho_vetor, tamanho_vetor);}
+void busca_binaria(std::vector<int> &vetor, int tamanho_vetor){busca_binaria(vetor, (tamanho_vetor + 1)/2, tamanho_vetor);}
+
