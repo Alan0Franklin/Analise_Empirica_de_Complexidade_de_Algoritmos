a67b7d768a66d483945bae8654c2841e99ff0cb2
Alan Franklin <167710977+Alan0Franklin@users.noreply.github.com>
2025-09-19 14:49:00 -0300

    Update main.cpp
    + Ordenação de vetor ocorre antes das duas funções de buscas;
    + "microsegundos" -> "nanosegundos".

--- src_cpp/main.cpp
+++ src_cpp/main.cpp
@@ -20,17 +20,17 @@ int main(){
     vector<long long> tempos_de_exec(4);
 
     tempos_de_exec[0] = cronometro_de_funcao(vetor, tamanho_vetor, bubblesort);
-    cout << "Tempo de saida do Bubblesort deu: " << tempos_de_exec[0] << " microsegundos." << endl;
+    cout << "Tempo de saida do Bubblesort deu: " << tempos_de_exec[0] << " nanosegundos." << endl;
 
     tempos_de_exec[1] = cronometro_de_funcao(vetor, tamanho_vetor, static_cast<void(*)(vector<int>&, int)>(mergesort));
-    cout << "Tempo de saida do Mergesort deu: " << tempos_de_exec[1] << " microsegundos." << endl;
-
-    tempos_de_exec[2] = cronometro_de_funcao(vetor, tamanho_vetor, static_cast<void(*)(vector<int>&, int)>(busca_sequencial));
-    cout << "Tempo de saida do Busca Sequencial deu: " << tempos_de_exec[2] << " microsegundos." << endl;
+    cout << "Tempo de saida do Mergesort deu: " << tempos_de_exec[1] << " nanosegundos." << endl;
 
     sort(vetor.begin(), vetor.end());
+    tempos_de_exec[2] = cronometro_de_funcao(vetor, tamanho_vetor, static_cast<void(*)(vector<int>&, int)>(busca_sequencial));
+    cout << "Tempo de saida do Busca Sequencial deu: " << tempos_de_exec[2] << " nanosegundos." << endl;
+
     tempos_de_exec[3] = cronometro_de_funcao(vetor, tamanho_vetor, static_cast<void(*)(vector<int>&, int)>(busca_binaria));
-    cout << "Tempo de saida do Busca Binaria deu: " << tempos_de_exec[3] << " microsegundos." << endl;
+    cout << "Tempo de saida do Busca Binaria deu: " << tempos_de_exec[3] << " nanosegundos." << endl;
 
     registrar_no_arquivo("Registro_-_Tempos_de_Execucao.csv", tamanho_vetor, nome_algoritmos, tempos_de_exec);
     return 0;
